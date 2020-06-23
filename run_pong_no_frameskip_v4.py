84ba65c4a1d62311e0b41a0a35553a098dc77688
Leejin Jung <jin.prelude@gmail.com>
2020-06-23 19:09:20 +0900

    init

--- run_pong_no_frameskip_v4.py
+++ run_pong_no_frameskip_v4.py
@@ -96,6 +96,12 @@ def parse_args() -> argparse.Namespace:
         action="store_false",
         help="turn off framestack",
     )
+    parser.add_argument(
+        "--save-experience",
+        dest="save_experience",
+        action="store_true",
+        help="save experience",
+    )
     return parser.parse_args()
 
 
